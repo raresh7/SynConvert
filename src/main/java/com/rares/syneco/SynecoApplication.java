2a051076296fb887978eaee4d88e46a4f7626652
raresh7 <raresroman7@yahoo.com>
2018-04-04 18:45:28 +0300

    initial empty application commit

---
+++ src/main/java/com/rares/syneco/SynecoApplication.java
@@ -0,0 +1,12 @@
+package com.rares.syneco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynecoApplication.class, args);
+	}
+}
