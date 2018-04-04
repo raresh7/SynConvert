dea5eeb3c17ae6dace7fcc04fb10620546dd9f03
raresh7 <raresroman7@yahoo.com>
2018-04-04 20:40:39 +0300

    converter works, added tests

--- src/main/java/com/rares/syneco/converter/SynConverter.java
+++ src/main/java/com/rares/syneco/converter/SynConverter.java
@@ -1,5 +1,8 @@
 package com.rares.syneco.converter;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +14,55 @@ public class SynConverter {
 	public String respondToInput(@RequestParam(name="input", defaultValue = "", required=true) String input) {
 		//verify input
 		//convert to appropriate output
-		return "salut" + input; //convert(input)
+		return convert(input); //convert(input)
 	}
 	
 	private String convert(String reqInput) {
 		
+		DateTimeFormatter yearFormatter = DateTimeFormat.forPattern("yyyy");
+		DateTimeFormatter quarterFormatter = DateTimeFormat.forPattern("yyyy-M");
+		DateTimeFormatter seasonMonthFormatter = DateTimeFormat.forPattern("yyyy-MM");
 		
-		return null;
+		DateTimeFormatter quarterOutFormatter = DateTimeFormat.forPattern("YY");
+		DateTimeFormatter monthOutFormatter = DateTimeFormat.forPattern("MMM-yyyy");
+		
+		DateTime tempDate;
+		StringBuilder res = new StringBuilder();
+		if(reqInput.startsWith("Q") ) {
+			tempDate = quarterFormatter.parseDateTime(getRelevantInput(reqInput));
+			res.append("Q");
+			res.append(tempDate.getMonthOfYear());
+			res.append('-');
+			res.append(quarterOutFormatter.print(tempDate));
+		} else if(reqInput.startsWith("SWS")) {
+			tempDate = seasonMonthFormatter.parseDateTime(getRelevantInput(reqInput));
+			res.append(getSeasonFromMonth(tempDate.getMonthOfYear()));
+			res.append('-');
+			res.append(quarterOutFormatter.print(tempDate));
+			if(res.toString().startsWith("Win")) {
+				res.append('/');
+				res.append(tempDate.getYearOfCentury() + 1);
+			} 
+		} else if(reqInput.startsWith("M")) {
+			tempDate = seasonMonthFormatter.parseDateTime(getRelevantInput(reqInput));
+			res.append(monthOutFormatter.print(tempDate));
+		}
+		else {
+			DateTime year = yearFormatter.parseDateTime(reqInput);
+			if(yearFormatter.print(year).equals(reqInput)) {
+				res.append(yearFormatter.print(year));
+			} else {
+				res.append("Incorrect input");
+			}
+		} 
+		return res.toString();
+	}
+	
+	private String getRelevantInput (String input) {
+		return input.substring(input.indexOf('-')+1);
+	}
+	
+	private String getSeasonFromMonth(int month) {
+		return month > 2 && month <9 ? "Sum" : "Win";
 	}
 }
--- src/test/java/com/rares/syneco/converter/SynConverterTest.java
+++ src/test/java/com/rares/syneco/converter/SynConverterTest.java
@@ -1,6 +1,6 @@
 package com.rares.syneco.converter;
 
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,9 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
-
-import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -19,9 +16,37 @@ public class SynConverterTest {
 
 	@Autowired
 	private SynConverter converter;
+	
+	@Test
+	public void converterTestYears() {
+		assertThat(converter.respondToInput("2017")).isEqualTo("2017");
+		assertThat(converter.respondToInput("2018")).isEqualTo("2018");
+		assertThat(converter.respondToInput("2019")).isEqualTo("2019");
+		assertThat(converter.respondToInput("2020")).isEqualTo("2020");
+	}
 
 	@Test
-	public void exampleTest() {
-		assertThat(converter.respondToInput("sf")).isEqualTo("salutsf");
+	public void converterTestQuarters() {
+		assertThat(converter.respondToInput("Q-2018-2")).isEqualTo("Q2-18");
+		assertThat(converter.respondToInput("Q-2018-3")).isEqualTo("Q3-18");
+		assertThat(converter.respondToInput("Q-2018-4")).isEqualTo("Q4-18");
+		assertThat(converter.respondToInput("Q-2019-1")).isEqualTo("Q1-19");
+		assertThat(converter.respondToInput("Q-2019-2")).isEqualTo("Q2-19");
+	}
+	
+	@Test
+	public void converterTestSeasons() {
+		assertThat(converter.respondToInput("SWS-2018-04")).isEqualTo("Sum-18");
+		assertThat(converter.respondToInput("SWS-2018-10")).isEqualTo("Win-18/19");
+		assertThat(converter.respondToInput("SWS-2019-04")).isEqualTo("Sum-19");
+		assertThat(converter.respondToInput("SWS-2019-10")).isEqualTo("Win-19/20");
+	}
+	
+	@Test
+	public void converterTestMonths() {
+		assertThat(converter.respondToInput("M-2018-02")).isEqualTo("Feb-2018");
+		assertThat(converter.respondToInput("M-2018-03")).isEqualTo("Mar-2018");
+		assertThat(converter.respondToInput("M-2018-04")).isEqualTo("Apr-2018");
+		assertThat(converter.respondToInput("M-2018-05")).isEqualTo("May-2018");
 	}
 }
